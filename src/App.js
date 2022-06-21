c2000c761e0e2acfb3c64c53808d889ceb7fbb87
Johan <joese6@gmail.com>
2022-06-21 09:54:20 +0700

    Tugas Pengenalan react

--- src/App.js
+++ src/App.js
@@ -2,12 +2,18 @@ import './App.css';
 
 // Uncomment untuk memuat daftar kontak
 // import contacts from './data/contacts.json';
-
+import Header from './components/Header';
+import Contact from './components/Contact';
+import contacts from './data/contacts.json';
 const App = () => {
   // Masukkan Header dan Contact ke dalam div App
+  const contactData=contacts.map(contact=>
+      <Contact data={contact} key={contact.name}/>
+    );
   return (
     <div className="App">
-      
+      <Header/>
+      {contactData}
     </div>
   );
 }
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,9 +2,18 @@
 // Kalian bisa membuat CSS sendiri di src/components/Contact.css
 import React from 'react';
 
-const Contact = () => {
+const Contact = (props) => {
     return (
-        <></>
+        <div className='contact'>
+        <div className='card-contact'>
+        <img src={props.data.photo} alt={props.data.name}/>
+            <div className='contact-data'>
+            <h3>{props.data.name}</h3>
+            <p>{props.data.phone}</p>
+            <p>{props.data.email}</p>
+            </div>
+        </div>
+        </div>
     )
 }
 
